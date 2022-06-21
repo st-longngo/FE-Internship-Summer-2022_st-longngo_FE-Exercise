3e6bdf956e156efaf81e14c4e13bf42bfad98032
st-longngo <long.ngo@supremetech.vn>
2022-06-21 09:53:26 +0700

    convert string es6 to es5

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,8 @@
 var products = getData(keyList.products, []);
 var storePage = document.getElementById('store-page');
+var cartLists = document.getElementById('cart-list');
+var orderLists = document.getElementById('order-list');
+var total = document.getElementById('total');
 
 window.addEventListener('DOMContentLoaded', function(e) {
   updateListCart();
@@ -7,91 +10,53 @@ window.addEventListener('DOMContentLoaded', function(e) {
 
 function renderListCart() {
   if(cart.length > 0) {
-    storePage.innerHTML = `
-    <section class="section section-store">
-      <div class="container">
-        <div class="row store-inner">
-          <div class="col-8 col-sm-12">
-            <h3 class="txt-bold txt-center cart-title">Shopping Cart</h3>
-            <ul class="cart-list" id="cart-list">
-              <li class="cart-row">
-                <span class="txt-bold cart-body">PRODUCT</span>
-                <span class="txt-bold cart-buttons">QUANTITY</span>
-                <span class="txt-center txt-bold cart-total">TOTAL</span>
-                <span class="cart-close"></span>
-              </li>
-            ${cart.map((item) => {
-              return `                
-              <li class="cart">
-                <div class="cart-body">
-                  <div class="cart-image">
-                    <img
-                      src="${item.image}"
-                      alt="${item.name}"
-                    />
-                  </div>
-                  <div class="cart-content">
-                    <h4 class="typo-2 txt-light cart-name">
-                      ${item.name}
-                    </h4>
-                    <span class="txt-regular cart-price">$${formatFixed(item.price - (item.price * item.discount / 100))}</span>
-                    ${item.discount ? `<span class="txt-regular cart-discount">$${item.price}</span>`: ``}
-                  </div>
-                </div>
-                <div class="cart-buttons" data-id=${item.id}>
-                  <button class="cart-btn" id="minus">
-                    <i class="bx bx-minus"></i>
-                  </button>
-                  <input type="text" class="cart-quantity" value=${item.quantity}>
-                  <button class="cart-btn" id="add">
-                    <i class="bx bx-plus"></i>
-                  </button>
-                </div>
-                <p class="txt-center cart-total">
-                $${formatFixed((item.price - (item.price * item.discount / 100)) * item.quantity)}
-                </p>
-                <button id="cart-close" class="cart-btn cart-close" data-id=${item.id}>
-                  <i class="bx bx-x"></i>
-                </button>
-              </li>
-              `
-            })}
-            </ul>
-          </div>
-          <div class="col-4 col-sm-12">
-            <h3 class="txt-bold txt-center cart-title">Sub Total</h3>
-            <ul class="order-list" id="order-list">
-              ${cart.map((item) => {
-                return `                
-                <li class="order-item">
-                  <h4 class="txt-light order-name">${item.name}</h4>
-                  <div class="txt-bold order-quantity">x${item.quantity}</div>
-                </li>`
-              }).join('')}
-            </ul>
-            <div class="order-total">
-              <p class="txt-bold">TOTAL PRICE</p>
-              <span class="total" id="total">
-                $${formatFixed(cart.reduce(function(acc, item){
-                    return acc + ((item.price - (item.price * item.discount / 100)) * item.quantity)
-                  },0))}
-              </span>
-            </div>
-            <a href="#" class="btn btn-secondary order-btn">check out</a>
-          </div>
-        </div>
-      </div>
-    </section>
-    `
+    cartLists.innerHTML = cart.map(function(item){
+      return "<li class='cart'>" +
+        "<div class='cart-body'>" +
+          "<div class='cart-image'>" +
+            "<img src=" + item.image + " alt='" + item.name + "'/>" +
+          "</div>" +
+          "<div class='cart-content'>" +
+            "<h4 class='typo-2 txt-light cart-name'>" + item.name + "</h4>" +
+            "<span class='txt-regular cart-price'>$" + formatFixed(item.price - (item.price * item.discount / 100)) + "</span>" +
+            (item.discount ? "<span class='txt-regular cart-discount'>$" + item.price + "</span>": "") +
+          "</div>" +
+        "</div>" +
+        "<div class='cart-buttons' data-id=" + item.id + ">" +
+          "<button class='cart-btn' id='minus'>" +
+            "<i class='bx bx-minus'></i>" +
+          "</button>" +
+          "<input type='text' class='cart-quantity' value=" + item.quantity + ">" +
+         " <button class='cart-btn' id='add'>" +
+            "<i class='bx bx-plus'></i>" +
+          "</button>" +
+        "</div>" +
+        "<p class='txt-center cart-total'>$" +
+        formatFixed((item.price - (item.price * item.discount / 100)) * item.quantity) +
+        "</p>" +
+        "<button id='cart-close' class='cart-btn cart-close' data-id=" + item.id + ">" +
+          "<i class='bx bx-x'></i>" +
+        "</button>" +
+      "</li>" 
+    }).join(' ');
+    orderLists.innerHTML = cart.map(function(item){
+      return "<li class='order-item'>" +
+        "<h4 class='txt-light order-name'>" + item.name + "</h4>" +
+        "<div class='txt-bold order-quantity'>x" + item.quantity + "</div>" +
+      "</li>"
+    }).join(' ');
+    total.innerHTML = "$" + formatFixed(cart.reduce(function(acc, item){
+      return acc + ((item.price - (item.price * item.discount / 100)) * item.quantity)
+    },0));
   } else {
     storePage.innerHTML = `
-    <section class="section section-error">
-      <div class="container">
-        <div class="error">
-          <div class="error-image">
-            <img src="./images/error-image.png" alt="Error 404" />
+    <section class='section section-error'>
+      <div class='container'>
+        <div class='error'>
+          <div class='error-image'>
+            <img src='./images/error-image.png' alt='Error 404' />
           </div>
-          <a href="index.html" class="btn btn-secondary">Back home</a>
+          <a href='index.html' class='btn btn-secondary'>Back home</a>
         </div>
       </div>
     </section>
--- js/product.js
+++ js/product.js
@@ -1,6 +1,7 @@
 var products = getData(keyList.products, []);
 var productLists = document.querySelectorAll('.product-list');
 
+
 window.addEventListener('DOMContentLoaded', function(e) {
   renderCartNumberOfListProduct();
   renderProduct();
@@ -9,39 +10,30 @@ window.addEventListener('DOMContentLoaded', function(e) {
 
 function renderProduct() {
   var listProduct = products.map(function(item) {
-      return `
-        <li class="col-3 col-sm-6">
-            <div class="product ${item.discount ? `product-sale` : ``}">
-              <div class="product-thumnail">
-                ${item.discount ? `<span class="badge badge-primary product-badge">-${item.discount}%</span>` : ""} 
-                <a href="#" class="product-link">
-                    <img
-                    src=${item.image}
-                    alt=${item.name}
-                    class="product-image"
-                    />
-                </a>
-                <div class="product-cart">
-                    <button class="btn btn-secondary btn-cart" data-id=${item.id}>add to cart</button>
-                </div>
-              </div>
-              <div class="product-content">
-                <h4 class="product-name">
-                    <a href="#" class="typo-2 txt-light">
-                        ${item.name}
-                    </a>
-                </h4>
-                <div class="product-price">
-                    ${
-                      item.discount ? `<span>$${formatFixed((item.price - (item.price * item.discount) / 100))}</span>
-                      <span class="product-discount">$${item.price}</span>` : `<span>$${item.price}</span>`
-                    }
-                </div>
-              </div>
-            </div>
-        </li>`;
-    })
-    .join("");
+    return "<li class='col-3 col-sm-6'>" +
+        "<div class='product " + (item.discount ? "product-sale" : " ") + " '>" +
+          "<div class='product-thumnail'>" + 
+            (item.discount ? "<span class='badge badge-primary product-badge'>" + "-" + item.discount + "%" + "</span>" : '') +
+            "<a href='#' class='product-link'>" +
+                "<img src=" + item.image + " alt='" + item.name + "' class='product-image' />" +
+            "</a>" +
+            "<div class='product-cart'>" +
+                "<button class='btn btn-secondary btn-cart' data-id=" + item.id + ">add to cart</button>" +
+            "</div>" +
+          "</div>" +
+          "<div class='product-content'>" +
+            "<h4 class='product-name'>" +
+                "<a href='#' class='typo-2 txt-light'>" + item.name + "</a>" +
+            "</h4>" +
+            "<div class='product-price'>" +
+                (item.discount ? "<span>$" + formatFixed((item.price - (item.price * item.discount) / 100)) + "</span>" +
+                  "<span class='product-discount'>$" + item.price + "</span>" : "<span>$" + item.price + "</span>"
+                ) +
+            "</div>" +
+          "</div>" +
+        "</div>" +
+      "</li>"; 
+  }).join(' ');
   productLists.forEach(function(productList) {
     productList.innerHTML = listProduct;
   });
